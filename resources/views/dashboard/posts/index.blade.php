0bf7a57762462ca69813c1168c4ea7082e993833
iqbalwaldan <iqbalwaldan.social@gmail.com>
2023-09-01 17:21:14 +0700

    Update & Delete Post

--- resources/views/dashboard/posts/index.blade.php
+++ resources/views/dashboard/posts/index.blade.php
@@ -30,8 +30,14 @@
                   <td>{{ $post->category->name }}</td>
                   <td>
                       <a href="/dashboard/posts/{{ $post->slug }}" class="badge bg-info"><i class="bi bi-eye"></i></a>
-                      <a href="" class="badge bg-warning"><i class="bi bi-pencil-square"></i></a>
-                      <a href="" class="badge bg-danger"><i class="bi bi-trash"></i></a>
+                      <a href="/dashboard/posts/{{ $post->slug }}/edit" class="badge bg-warning"><i class="bi bi-pencil-square"></i></a>
+                      <form action="/dashboard/posts/{{ $post->slug }}" method="POST" class="d-inline">
+                        @method('delete')
+                        @csrf
+                        <button class="badge bg-danger border-0" onclick="return confirm('Are you sure?')">
+                          <i class="bi bi-trash"></i>
+                        </button>
+                      </form>
                   </td>
               </tr>
           @endforeach
