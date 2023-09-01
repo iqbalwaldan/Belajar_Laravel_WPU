c5148f89fc15b40c18ed3b2cefd69e9bf76e5d4c
iqbalwaldan <iqbalwaldan.social@gmail.com>
2023-09-01 09:30:05 +0700

    Dashboard UI

---
+++ resources/views/dashboard/layouts/main.blade.php
@@ -0,0 +1,38 @@
+<!doctype html>
+<html lang="en" data-bs-theme="auto">
+    <head><script src="../assets/js/color-modes.js"></script>
+
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>WPU Blog | Dashboard</title>
+
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-4bw+/aepP/YC94hEpVNVgiZdgIC5+VKNBQNGCHeKRQN+PtmoHDEXuppvnDJzQIu9" crossorigin="anonymous">
+
+
+        
+        <!-- Custom styles for this template -->
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.5/font/bootstrap-icons.css">
+        <!-- Custom styles for this template -->
+        <link href="css/dashboard.css" rel="stylesheet">
+
+    </head>
+    <body>
+
+        @include('dashboard.layouts.header')
+
+        <div class="container-fluid">
+            <div class="row">
+                @include('dashboard.layouts.sidebar')
+
+                <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
+                    @yield('container')
+                </main>
+            </div>
+        </div>
+
+
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-HwwvtgBNo3bZJJLYd8oVXjrBZt8cqVSpeBNS5n7C8IVInixGAoxmnlMuBnhbgrkm" crossorigin="anonymous"></script>
+
+        <script src="/js/dashboard.js"></script>
+    </body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,6 @@
 <?php
 
-use App\Http\Controllers\DashboardController;
+use App\Http\Controllers\DashboardPostController;
 use App\Models\Category;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PostController;
@@ -71,4 +71,6 @@ Route::get('/logout',[LoginController::class, 'logout']);
 Route::get('/register',[RegisterController::class, 'index'])->middleware('guest');
 Route::post('/register',[RegisterController::class, 'store']);
 
-Route::get('/dashboard',[DashboardController::class, 'index'])->middleware('auth');
+Route::get('/dashboard',fn()=>view('dashboard.index'))->middleware('auth');
+
+Route::resource('/dashboard/posts', DashboardPostController::class)->middleware('auth');
